f18ee191c1038699cf9aface386334adfe2f5b2d
respektan <n.respekta@gmail.com>
2018-10-10 17:31:04 +0200

    Initial commit

---
+++ src/main/java/pi/infoshare/App.java
@@ -0,0 +1,19 @@
+package pi.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main ( String[] args )
+{
+    int dodawanie = 2 + 2;
+    int odejmowanie = 2 - 2;
+    /*int dzielenie = 2 / 0;*/
+    System.out.println( dodawanie );
+    System.out.println( odejmowanie );
+    /*System.out.println( dzielenie ); */
+    }
+}
+
